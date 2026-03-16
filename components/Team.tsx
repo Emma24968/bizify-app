84335c2d63a10518d547330af4d40a4dcf963679
Emma24968 <emnabs12@gmail.com>
2026-03-16 16:49:49 +0100

    Fixed the background and text color for the team section and also fixes the margin overlap

--- components/Team.tsx
+++ components/Team.tsx
@@ -6,15 +6,15 @@ import React from "react";
 export default function Team() {
   return (
     <div>
-      <section className="justify-items-center mb-[4rem]">
-        <h3 className="text-5xl font-bold text-center my-15">
+      <section className="justify-items-center dark:bg-white text-black pb-[4rem]">
+        <h3 className="text-5xl font-bold text-center py-15">
           Our team is always <br /> here for you
         </h3>
         <div className="flex gap-9 ">
           {teams.map((team, index) => {
             return (
               <div key={index} className="rounded-lg group relative">
-                <div className="absolute ml-4 mt-5 text-[#191818]  opacity-0 group-hover:opacity-100 transition duration-300 flex flex-col gap-3">
+                <div className="absolute ml-4 mt-5 text-[#191818]   opacity-0 group-hover:opacity-100 transition duration-300 flex flex-col gap-3">
                   <a href="#">
                     {" "}
                     <Linkedin />
@@ -33,7 +33,7 @@ export default function Team() {
                   className="rounded-t-xl"
                   width={250}
                 />
-                <div className="text-center rounded-b-xl bg-[#e3ebf3d4]">
+                <div className="text-center rounded-b-xl bg-[#e3ebf3d4] pb-1">
                   <p className=" text-[15px] font-bold">{team.name}</p>
                   <p>{team.position}</p>
                 </div>
