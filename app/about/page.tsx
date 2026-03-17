087261f091586295921d90add9bb33abd251e0ae
Emma24968 <emnabs12@gmail.com>
2026-03-17 02:58:13 +0100

    Created a component for the about team and fixed the layout and darkmode color

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,6 +18,7 @@ import Team from "@/public/Team4.jpg";
 import Image from "next/image";
 import About from "@/components/about/About";
 import AboutIntro from "@/components/about/AboutIntro";
+import AboutTeam from "@/components/about/AboutTeam";
 
 export default function AboutPage() {
   return (
@@ -33,43 +34,7 @@ export default function AboutPage() {
       </section>
       <About />
       <AboutIntro />
-      <section className="justify-items-center mb-[4rem]">
-        <h3 className="text-[54px] font-bold text-center my-15">
-          Our team is always <br /> here for you
-        </h3>
-        <div className="flex gap-9 ">
-          {members.map((team, index) => {
-            return (
-              <div key={index} className="rounded-lg group relative">
-                <div className="text-center rounded-t-xl px-4 pt-4 rounded-b-xl bg-[#e3ebf3d4]">
-                  <Image
-                    src={team.image}
-                    alt={team.name}
-                    className="rounded-t-xl"
-                    width={220}
-                  />
-                  <p className=" text-[15px] font-bold">{team.name}</p>
-                  <p>{team.position}</p>
-                  <div className="flex gap-1 justify-center opacity-0 group-hover:opacity-100 transition duration-300 pb-4">
-                    <div className="bg-white hover:bg-[#84A17D] p-1 w-[36px] content-center h-[36px] rounded-[2px] text-black hover:text-white text-[13px] cursor-pointer">
-                      <a href="#">FB</a>
-                    </div>
-                    <div className="bg-white hover:bg-[#84A17D] p-1 w-[36px] content-center h-[36px] rounded-[2px] text-black hover:text-white text-[13px] cursor-pointer">
-                      <a href="#">TW</a>
-                    </div>
-                    <div className="bg-white hover:bg-[#84A17D] p-1 w-[36px] content-center h-[36px] rounded-[2px] text-black hover:text-white text-[13px] cursor-pointer">
-                      <a href="#">IG</a>
-                    </div>
-                    <div className="bg-white hover:bg-[#84A17D] p-1 w-[36px] content-center h-[36px] rounded-[2px] text-black hover:text-white text-[13px] cursor-pointer">
-                      <a href="#">YT</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </section>
+      <AboutTeam />
       <section className="py-16 px-16 bg-[#153C3C]">
         <div className="flex justify-between items-center mb-12">
           <h3 className="text-[54px] font-bold">
---
+++ components/about/AboutTeam.tsx
@@ -0,0 +1,47 @@
+import { members } from '@/app/data';
+import React from 'react'
+import Image from 'next/image';
+export default function AboutTeam() {
+  return (
+    <div>
+              <section className="justify-items-center dark:bg-white text-black pb-[4rem]">
+                <h3 className="text-[54px] font-bold text-center py-15">
+                  Our team is always <br /> here for you
+                </h3>
+                <div className="flex gap-9 ">
+                  {members.map((team, index) => {
+                    return (
+                      <div key={index} className="rounded-lg group relative">
+                        <div className="text-center rounded-t-xl px-4 pt-4 rounded-b-xl bg-[#e3ebf3d4]">
+                          <Image
+                            src={team.image}
+                            alt={team.name}
+                            className="rounded-t-xl"
+                            width={220}
+                          />
+                          <p className=" text-[15px] font-bold">{team.name}</p>
+                          <p>{team.position}</p>
+                          <div className="flex gap-1 justify-center opacity-0 group-hover:opacity-100 transition duration-300 pb-4">
+                            <div className="bg-white hover:bg-[#84A17D] p-1 w-[36px] content-center h-[36px] rounded-[2px] text-black hover:text-white text-[13px] cursor-pointer">
+                              <a href="#">FB</a>
+                            </div>
+                            <div className="bg-white hover:bg-[#84A17D] p-1 w-[36px] content-center h-[36px] rounded-[2px] text-black hover:text-white text-[13px] cursor-pointer">
+                              <a href="#">TW</a>
+                            </div>
+                            <div className="bg-white hover:bg-[#84A17D] p-1 w-[36px] content-center h-[36px] rounded-[2px] text-black hover:text-white text-[13px] cursor-pointer">
+                              <a href="#">IG</a>
+                            </div>
+                            <div className="bg-white hover:bg-[#84A17D] p-1 w-[36px] content-center h-[36px] rounded-[2px] text-black hover:text-white text-[13px] cursor-pointer">
+                              <a href="#">YT</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </section>
+        
+    </div>
+  )
+}
