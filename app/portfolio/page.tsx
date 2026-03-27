8ca3d46258a9f071930047c635f2ea63b02ed27c
Emma24968 <emnabs12@gmail.com>
2026-03-27 03:56:21 +0100

    Created the portfolio page

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,10 @@
+import PageHeader from '@/app/PageHeader'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <PageHeader title='Portfolio' currentPage='Portfolio' />
+    </div>
+  )
+}
