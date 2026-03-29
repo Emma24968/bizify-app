5520bb31257830c104df0d3d7b6d92c7fab444fd
Emma24968 <emnabs12@gmail.com>
2026-03-29 05:23:43 +0100

    Styled the heading in the pricing component

--- components/services/Pricing.tsx
+++ components/services/Pricing.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import PricingCard from "./PricingCard";
 import { pricingPlans } from "@/app/data";
 
-export default function Pricing() {
+export default function  Pricing() {
   return (
     <div className="bg-white">
-      <h3>
+      <h3 className="text-4xl font-semibold text-center pt-4">
         Your best pricing <br /> offer awaits
       </h3>
       <section className="grid md:grid-cols-3 gap-6 px-10 py-20 ">
