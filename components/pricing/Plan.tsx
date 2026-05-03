1c24a15bbf97750ed07c279ed6bb1f59402b429a
Emma24968 <emnabs12@gmail.com>
2026-05-03 18:16:58 +0100

    Created the FAQ section

---
+++ components/pricing/Plan.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Plan() {
+  return (
+    <div>
+        <div className=""></div>
+        <div className="">
+            <h3>Frequently asked asked question</h3>
+            <p>Morem ipsum dolor sit amet, consectetur adipiscing elita florai psum dolor sit amet,consectetur amet consecteture.</p>
+        </div>
+    </div>
+  )
+}
