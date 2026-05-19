2a58ea209c0f4b2061e7d8e08fe6b13707aa3ec3
Emma24968 <emnabs12@gmail.com>
2026-05-19 12:23:08 +0100

    Moved the contactform file to the contact folder

---
+++ components/contact/Detail.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Detail() {
+  return (
+    <div>
+        <h3>Make something awesome.</h3>
+        <p>Each demo built with Teba will look different. You can customize almost anything in the appearance of your website with only a few clicks. Each demo built with Teba will look different.</p>
+        {}
+    </div>
+  )
+}
