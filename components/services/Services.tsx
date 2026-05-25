bdac7406d909984224b87d5d0cf412a245960e9c
Emma24968 <emnabs12@gmail.com>
2026-05-26 00:00:06 +0100

    Fixed conflict

--- components/services/Services.tsx
+++ components/services/Services.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import Image from "next/image";
-<<<<<<< HEAD
-import Link from "next/link"; // 1. Import Link
-=======
 import Link from "next/link";
->>>>>>> pricing
 import { serviceCards } from "@/app/data";
 import { serviceList } from "@/app/data";
 import { ArrowRight } from "lucide-react";
 
 export default function Services() {
   return (
-    <div className="grid px-16 py-16 grid-cols-3 justify-center">
+    <div className="grid px-16 py-16 grid-cols-3 justify-items-center">
       {serviceCards.map((service, index) => {
         return (
           <div
@@ -24,18 +20,8 @@ export default function Services() {
               width={70}
               className=" hover:text-white p-2 rounded-[3px] bg-[#84a17d]"
             />
-            <h3 className="font-bold">{service.title}</h3>
+            <h3>{service.title}</h3>
             <p>{service.description}</p>
-<<<<<<< HEAD
-            
-            {/* 2. Wrap the button/arrow in a Link tag */}
-            <Link 
-              href={`/services/${service.id}`} 
-              className="flex items-center group cursor-pointer"
-            >
-              <button className="text-[#84a17d] font-medium cursor-pointer">Learn more</button>
-              <ArrowRight className="text-[#84a17d] ml-1 transition-transform group-hover:translate-x-1" />
-=======
             <Link href={`/services/${service.id.toLowerCase()}`}>
               <div className="flex items-centergroup ">
                 <button className="text-[#84a17d] cursor-pointer  text-[12px] gap-2 hover:text-black">
@@ -46,7 +32,6 @@ export default function Services() {
                   className="text-[#84a17d] cursor-pointer  hover:text-black"
                 />
               </div>
->>>>>>> pricing
             </Link>
           </div>
         );
